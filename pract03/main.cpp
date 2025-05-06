77150521b8f6ef511731fe3cdb6ce838fbca7b99
yashbhoomkar <bhoomkar04@gmail.com>
2025-05-06 08:15:36 +0000

    Prims MST code added

--- pract03/main.cpp
+++ pract03/main.cpp
@@ -42,9 +42,90 @@ class SelectionSort
 };
 
 
-int main()
+class PrimsAlgorithmMST
+{
+    public:
+        vector<vector<pair<int, int>>> adj;  // {vertex, weight}
+        int V , E;
+
+        void primsMST()
+        {
+            // {weight , vertex , parent}
+            priority_queue<vector<int>, vector<vector<int>>, greater<vector<int>>> pq;
+            vector<int> visited(V, 0);
+            pq.push({0 , 0 , -1}); // Starting with vertex 0
+
+            while(!pq.empty())
+            {
+                vector<int> curr = pq.top();
+                pq.pop();
+
+                int weight = curr[0];
+                int vertex = curr[1];
+                int parent = curr[2];
+
+                if(visited[vertex] == 1)
+                {
+                    continue;
+                }
+
+                // Mark vertex as visited
+                visited[vertex] = 1;
+
+                if(parent != -1)
+                {
+                    cout << parent << " - " << vertex << " : " << weight << endl;
+                }
+
+                // Explore all adjacent vertices of the current vertex
+                for(auto edge : adj[vertex])
+                {
+                    int v = edge.first;
+                    int w = edge.second;
+                    if (!visited[v])
+                    {
+                        pq.push({w, v, vertex});
+                    }
+                }
+            }
+        }
+
+        int main()
+        {
+            // adj -> {vertex, weight}
+            vector<vector<pair<int, int>>> adj = {
+                {{1, 2}, {2, 1}},        // Vertex 0
+                {{0, 2}, {2, 1}},        // Vertex 1
+                {{0, 1}, {1, 1}, {3, 2}, {4, 2}}, // Vertex 2
+                {{2, 2}, {4, 1}},        // Vertex 3
+                {{2, 2}, {3, 1}}         // Vertex 4
+            };
+
+            this->adj = adj;
+            this->V = 5;
+            this->E = 6;
+
+            primsMST();
+
+            return 0;
+        }
+};
+
+void selectionSort()
 {
     SelectionSort obj;
     obj.main();
+}
+
+void primsAlgorithmMST()
+{
+    PrimsAlgorithmMST obj;
+    obj.main();
+}
+
+int main()
+{
+    //selectionSort();
+    primsAlgorithmMST();
     return 0;
 }
